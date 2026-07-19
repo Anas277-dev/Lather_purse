2a1b0e5af03ab365605713b0438c5b1600f7452a
Anas277-dev <muhammadanassid123@gmail.com>
2026-07-20 01:07:54 +0500

    node env production in index.js

--- backend/index.js
+++ backend/index.js
@@ -89,13 +89,13 @@ app.use('/api/settings', siteSettingsRoutes);
 app.use('/api/analytics', analyticsRoutes);
 
 // Serve frontend in production
-if (NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+// if (NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+//   });
+// }
 
 // 404 handler
 app.use((req, res) => {
